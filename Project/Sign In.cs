732e2059ef476ee6e3f56017188d83cadd6e273e
Sasha Fedorienko <Sasha Fedorienko@DESKTOP-TI5J653>
2025-03-09 01:03:57 +0200

    signup2

--- Project/Sign In.cs
+++ Project/Sign In.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,7 @@ namespace Project
 {
     public partial class Auth : Form
     {
+        DataBase database = new DataBase();
         public Auth()
         {
             InitializeComponent();
@@ -25,6 +27,27 @@ namespace Project
             var passUser = textBox_Psw.Text;
 
             SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"select id_user, login_user, password_user from users where login_user = '{loginUser}' and password_user = '{passUser}'";
+
+            SqlCommand command = new SqlCommand(querystring, database.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 1)
+            {
+                MessageBox.Show("You are logged in");
+                Form1 frm1 = new Form1();
+                this.Hide();
+                frm1.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Login or password is incorrect");
+            }
         }
     }
 }

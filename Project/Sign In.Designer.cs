e3339cf86401aca4cce81cc978d3a3ee7895ea33
Sasha Fedorienko <Sasha Fedorienko@DESKTOP-TI5J653>
2025-03-09 00:50:49 +0200

    signupcode1

--- Project/Sign In.Designer.cs
+++ Project/Sign In.Designer.cs
@@ -28,66 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox_Login = new System.Windows.Forms.RichTextBox();
+            this.textBox_Psw = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.signUpBtn = new System.Windows.Forms.Button();
+            this.btnEnterSignIn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // textBox_Login
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(34, 119);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(399, 59);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
+            this.textBox_Login.Location = new System.Drawing.Point(26, 97);
+            this.textBox_Login.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_Login.Name = "textBox_Login";
+            this.textBox_Login.Size = new System.Drawing.Size(300, 49);
+            this.textBox_Login.TabIndex = 1;
+            this.textBox_Login.Text = "";
             // 
-            // richTextBox2
+            // textBox_Psw
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(34, 228);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(399, 59);
-            this.richTextBox2.TabIndex = 2;
-            this.richTextBox2.Text = "";
+            this.textBox_Psw.Location = new System.Drawing.Point(26, 185);
+            this.textBox_Psw.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_Psw.Name = "textBox_Psw";
+            this.textBox_Psw.Size = new System.Drawing.Size(300, 49);
+            this.textBox_Psw.TabIndex = 2;
+            this.textBox_Psw.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(80, 12);
+            this.button1.Location = new System.Drawing.Point(60, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 28);
+            this.button1.Size = new System.Drawing.Size(92, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "SIGN IN";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // signUpBtn
             // 
-            this.button2.Location = new System.Drawing.Point(260, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(122, 28);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "SIGN UP";
-            this.button2.UseVisualStyleBackColor = true;
+            this.signUpBtn.Location = new System.Drawing.Point(195, 10);
+            this.signUpBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.signUpBtn.Name = "signUpBtn";
+            this.signUpBtn.Size = new System.Drawing.Size(92, 23);
+            this.signUpBtn.TabIndex = 6;
+            this.signUpBtn.Text = "SIGN UP";
+            this.signUpBtn.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnEnterSignIn
             // 
-            this.button3.Location = new System.Drawing.Point(34, 493);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(398, 44);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "SIGN IN";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnEnterSignIn.Location = new System.Drawing.Point(26, 401);
+            this.btnEnterSignIn.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEnterSignIn.Name = "btnEnterSignIn";
+            this.btnEnterSignIn.Size = new System.Drawing.Size(298, 36);
+            this.btnEnterSignIn.TabIndex = 7;
+            this.btnEnterSignIn.Text = "SIGN IN";
+            this.btnEnterSignIn.UseVisualStyleBackColor = true;
+            this.btnEnterSignIn.Click += new System.EventHandler(this.btnEnterSignIn_Click);
             // 
             // Auth
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 603);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(362, 490);
+            this.Controls.Add(this.btnEnterSignIn);
+            this.Controls.Add(this.signUpBtn);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox_Psw);
+            this.Controls.Add(this.textBox_Login);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Auth";
             this.Text = "Sign In";
             this.ResumeLayout(false);
@@ -95,10 +102,10 @@
         }
 
         #endregion
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox textBox_Login;
+        private System.Windows.Forms.RichTextBox textBox_Psw;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button signUpBtn;
+        private System.Windows.Forms.Button btnEnterSignIn;
     }
 }
--- Project/Sign In.cs
+++ Project/Sign In.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,15 @@ namespace Project
         public Auth()
         {
             InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void btnEnterSignIn_Click(object sender, EventArgs e)
+        {
+            var loginUser = textBox_Login.Text;
+            var passUser = textBox_Psw.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
         }
     }
 }

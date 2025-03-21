6e31afc20eb2c4fe90ce0bb42dfd401fd61c64d3
Sasha Fedorienko <Sasha Fedorienko@DESKTOP-TI5J653>
2025-03-21 19:42:13 +0200

    form1

--- Project/Form1.Designer.cs
+++ Project/Form1.Designer.cs
@@ -145,7 +145,7 @@ namespace Project
             // 
             this.buttonAdminPanel.BackColor = System.Drawing.Color.SeaGreen;
             this.buttonAdminPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonAdminPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdminPanel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonAdminPanel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.buttonAdminPanel.Location = new System.Drawing.Point(26, 1027);
             this.buttonAdminPanel.Name = "buttonAdminPanel";
